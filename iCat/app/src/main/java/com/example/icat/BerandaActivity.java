e282f827537edd19efb31ffa637e1aefb4a88d49
ivanandika21 <ivanandikasurya21@gmail.com>
2022-02-27 09:58:31 +0700

    carousel gagal

--- iCat/app/src/main/java/com/example/icat/BerandaActivity.java
+++ iCat/app/src/main/java/com/example/icat/BerandaActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 public class BerandaActivity extends AppCompatActivity {
 
     private TextView txtHello;
+    private ImageButton carouselBefore, carouselNext;
+    private ImageSwitcher carouselImage;
+
+    int index = 0;
+    int indexImg[] = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,47 @@ public class BerandaActivity extends AppCompatActivity {
         user = intent.getStringExtra("user");
 
         txtHello.append(user);
+
+
+        // Codingan carousel
+        carouselBefore = findViewById(R.id.carouselBefore);
+        carouselNext = findViewById(R.id.carouselNext);
+        carouselImage = findViewById(R.id.carouselImage);
+
+        carouselBefore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                --index;
+                if (index < 0){
+                    index = indexImg.length-1;
+                }
+                carouselImage.setImageResource(indexImg[index]);
+            }
+        });
+
+        carouselNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index++;
+                if (index == indexImg.length){
+                    index = 0;
+                }
+                carouselImage.setImageResource(indexImg[index]);
+            }
+        });
+
+        carouselImage.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//                imageView.setMaxWidth(250);
+//                imageView.setMaxHeight(250);
+
+                return imageView;
+            }
+        });
+
+        carouselImage.setImageResource(indexImg[index]);
     }
 }
